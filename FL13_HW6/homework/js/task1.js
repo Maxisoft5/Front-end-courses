1f36ac052c5fa5d5a8460e983371f9269358a902
Maxim Grynyuk <52629630+Maxisoft5@users.noreply.github.com>
2020-04-23 20:28:24 +0300

    HW6!

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -8,6 +8,9 @@
        alert('invalid input data');
        continue label1;
     }
+    if(checkNumber === null){
+      continue label1;
+    }
   
     label2: while (x){
   
@@ -17,17 +20,22 @@
     || tipPercentage === '' || tipPercentage === ' '){
       alert('invalid input data');
       continue label2;
-  
+     
+    }
+    if(checkNumber === null){
+      continue label2;
     }else{
   
-    let tipAmount;
-    let totalSum;
+    let tipAmount = 0;
+    let totalSum = 0;
     checkNumber = Number(checkNumber);
     tipPercentage = Number(tipPercentage);
     tipAmount = checkNumber * (tipPercentage * 0.01);
-    totalSum = tipAmount + checkNumber; 
-    alert(` Check number: ${checkNumber}, \n Tip: ${tipPercentage},
- Tip amount: ${tipAmount}, \n Total sum to pay: ${totalSum}`);
+    tipAmount = Number(tipAmount.toFixed(2));
+    totalSum = tipAmount + checkNumber;
+
+    alert(` Check number: ${checkNumber} \n Tip: ${tipPercentage}
+ Tip amount: ${tipAmount} \n Total sum to pay: ${totalSum}`);
      break label1;
     }
   }
