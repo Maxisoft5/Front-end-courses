b80c4efd7a9732054e8ccb7ad6a7aaf5d2c32306
Maxim Grynyuk <52629630+Maxisoft5@users.noreply.github.com>
2020-05-12 23:42:39 +0300

    HW11

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -53,23 +53,159 @@ const data = [
 const rootNode = document.getElementById('root');
 // TODO: your code goes here
 
+function createTree(array, obj) {
+  array.append(createTreeDom(obj));
+}
+function createTreeDom(obj) {
 
+  let ul = document.createElement('ul');
 
-function createTree(container, obj) {
-  container.append(createTreeText(obj));
+  for (let el of obj) {
+    let li = document.createElement('li');
+    if(el.folder){
+      li.dataset.type = 'folder';
+      li.dataset.isOpen = 'false';
+      li.innerHTML = '<span class="material-icons folder">folder</span>'+ ' ' + el.title;
+    }else{
+      li.dataset.type = 'file';
+      li.innerHTML ='<span class="material-icons file">insert_drive_file</span>' + ' ' + el.title;
+    }
+    if(el.children === null){
+      li.dataset.children = 'false';
+    }
+    if(obj[obj.indexOf(el)].children){
+    let childrenUl = createTreeDom(obj[obj.indexOf(el)].children);
+    li.append(childrenUl);
+    } 
+    ul.append(li);
+  }
+
+  return ul;
+}
+createTree(rootNode, data);
+
+function showChildren(e){
+  const target = e.target;
+  if(target.lastChild.tagName !== 'UL') {
+    if(target.dataset.children === 'false'){
+      const ul = document.createElement('ul');
+      let li = document.createElement('li');
+      li.innerHTML = 'Folder is empty';
+      target.append(ul);
+      ul.append(li);
+    }else {
+      return;
+    }
+  }
+  if(target.dataset.type === 'folder'){
+    target.firstChild.innerHTML = '<span class="material-icons folder">folder_open</span>';
+  }
+
+  target.lastChild.style.display = 'block';
+  target.dataset.isOpen = 'true';
+}
+function hideChilren(e){
+  const target = e.target;
+
+  if (target.dataset.type !== 'folder') {
+    return;
+  }
+
+  target.firstChild.innerHTML = '<span class="material-icons folder">folder</span>';
+  target.lastChild.style.display = 'none';
+  target.dataset.isOpen = 'false';
 }
 
-function createTreeText(obj) { // отдельная рекурсивная функция
-  let li = '';
-  let ul;
-  for (let key in obj) {
-    li += '<li>' + key + createTreeText(obj[key]) + '</li>';
+function toogleFolder(e) {
+  contextMenuClose();
+
+  if (e.target.dataset.isOpen === 'true') {
+    return hideChilren(e);
   }
-  if (li) {
-    ul = '<ul>' + li + '</ul>'
+
+  if (e.target.dataset.isOpen === 'false') {
+    return showChildren(e);
   }
-  return ul || '';
 }
 
-createTree(rootNode,data)
+let curItem;
+function openContextMenu (e) {
+  e.preventDefault();
+  contextMenuClose();
+
+  if (e.target.dataset.type === 'folder' || e.target.dataset.type === 'file'){
+    createContextMenu(e);
+  }
+}
+
+function createContextMenu(e) {
+  curItem = e.target;
+  curItem.style.backgroundColor = 'lightblue';
+  const pageX = e.clientX + 17;
+  const pageY = e.clientY - 20;
+  let ul = document.createElement('ul');
+  ul.classList.add('context-menu');
+  ul.id = 'contextMenu';
+  let liEdit = document.createElement('li');
+  liEdit.id = 'liEdit';
+  liEdit.innerHTML = 'Rename';
+  let liDelete = document.createElement('li');
+  liDelete.id = 'liDelete';
+  liDelete.innerHTML = 'Delete item';
+  ul.style.left = pageX + 'px';
+  ul.style.top = pageY + 'px';
+  ul.append(liEdit);
+  ul.append(liDelete);
+  document.body.append(ul);
+  liDelete.onclick = deleteItem;
+  liEdit.onclick = editItem;
+}
+
+function deleteItem(e) {
+  if(curItem.parentNode.children.length === 1){
+    curItem.innerHTML = 'Folder is empty';
+  }else{
+    curItem.parentNode.removeChild(curItem);
+  }
+  contextMenuClose(e);
+}
+
+function editItem() {
+  const title = curItem.childNodes[1];
+  const text = title.textContent;
+
+  curItem.removeChild(title);
+
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('value', text);
+  input.style.display = 'inline-block';
+  input.onblur = (e) => {
+    const newText = document.createTextNode(e.target.value)
+    curItem.removeChild(e.target)
+    curItem.childNodes[0].parentNode.insertBefore(newText, curItem.childNodes[0].nextSibling);
+  };
+  curItem.childNodes[0].parentNode.insertBefore(input, curItem.childNodes[0].nextSibling);
+  input.focus();
+  input.setSelectionRange(0, text.split('.')[0].length);
+  contextMenuClose();
+}
+
+function contextMenuClose() {
+  const menu = document.getElementById('contextMenu');
+  if (menu) {
+    menu.outerHTML = '';
+  }
+}
+rootNode.onmouseover = function(e){
+  if(e.target.tagName !== 'LI') {
+    return;
+  }
+  e.target.style.backgroundColor = 'lightblue';
+}
+rootNode.onmouseout = function(e){
+  e.target.style.backgroundColor = 'white';
+}
+rootNode.onclick = toogleFolder;
+rootNode.oncontextmenu = openContextMenu;
 
