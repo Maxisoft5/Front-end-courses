dbb6686baf6b9da9bf4a6071af1c66bacdd103de
WebProjectGroup <WebProjectGroup@i.ua>
2020-06-02 12:39:07 +0300

    HW8

--- FL13_HW8/homework/js/countPoints.js
+++ FL13_HW8/homework/js/countPoints.js
@@ -1,14 +1,18 @@
-console.log(countPoints('3:1','3:3','4:3','1:2'));
-function countPoints(){
-  let points = 0;
-  for(let i = 0; i < arguments.length; i++){
-  let firstScore = String(arguments[i]).charAt(0);
-  let secondScore = String(arguments[i]).charAt(2);
-    if(firstScore > secondScore){
-    points += 3;
-    }else if(firstScore === secondScore){
-    points += 1;
-    }
-  }
-   return points;
+function isBigger(a,b){
+	return a > b;
 }
+function countPoints(matchesRezults){
+	let sum=0
+	matchesRezults.forEach(function(point, game){
+		if(point[0]>=0 && point[0]<=4 && point[2]>=0 && point[2]<=4 && game<10){
+			if(isBigger(point[0],point[2])){
+				sum+=3;
+			} else if(point[0]===point[2]){
+				sum+=1;
+			}
+		}
+	});
+	return sum;
+}
+console.log(countPoints(['3:1', '1:0', '0:0', '1:2', '4:0', '2:3', '1:1', '0:1', '2:1', '1:0']));
+console.log(countPoints(['1:1', '1:2', '2:0', '4:2', '0:1', '2:3', '1:1', '0:1', '1:1', '3:0']));
--- FL13_HW8/homework/js/getDifference.js
+++ FL13_HW8/homework/js/getDifference.js
@@ -1,10 +1,13 @@
-function getDifference(firstNum, secondNum){
-  let result;
-  if(secondNum > firstNum){
-      result = secondNum - firstNum;
-    }else{
-      result = firstNum - secondNum;
-    }
-    return result;
+function isBigger(a,b){
+	return a > b;
 }
-console.log(getDifference(-3, -14));
+function getDifference(a,b){
+	if(isBigger(a,b)){
+		return a-b;
+	} else{
+		return b-a;
+	}
+}
+console.log(`getDifference 5 4 : ${getDifference(5,4)} \n
+	getDifference 4 5 : ${getDifference(4,5)} \n
+	getDifference 5 5 : ${getDifference(5,5)} `);
