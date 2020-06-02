32931471f26a66350ebffea6a0a8aa4ee5a1609e
WebProjectGroup <WebProjectGroup@i.ua>
2020-06-02 12:36:16 +0300

    HW10

--- FL13_HW10/homework/js/fighters_game.js
+++ FL13_HW10/homework/js/fighters_game.js
@@ -1,121 +1,97 @@
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 class Fighter {
-    constructor(fighter){
-        let wins = 0;
-        let losses = 0;
-        this._name = fighter.name;
-        this._health = fighter.hp;
-        this._damage = fighter.damage;
-        this._strength = fighter.strength;
-        this._agility = fighter.agility;
-        this._wins = wins;
-        this._losses = losses;
-    }
-    getName() {
-        return this._name;
-    }
-    getDamage() {
-        return this._damage;
-    }
-    getHealth() {
-        return this._health;
-    }
-    get health(){
-        return this._health;
-    }
-    set health(value) {
-        this._health = value;
-    }
-    getStrength() {
-        return this._strength;
-    }
-    getAgility () {
-        return this._agility;
-    }
-    getWins() {
-        return this._wins;
-    }
-    addWins () {
-        return ++this._wins;
-    }
-    getLosses () {
-        return this.losses;
-    }
-    addLosses () {
-        return ++this.losses;
-    }
-    get wins() {
-        return this._wins;
-    }
-    set wins(value) {
-        this._wins = value;
-    }
-    get losses() {
-        return this._losses;
-    }
-    set losses(value) {
-        this._losses = value;
-    }
-    attack(fighter){
-        if(fighter === this){
-            return console.log(`no suicide pls'`);
-        }
-        let successPercent;
-        let randomPercent = Math.round(Math.floor(Math.random() * 100) + 1);
-        successPercent = Math.round(100 - (fighter.getStrength() * 50 / 60 + fighter.getAgility() * 50 / 40));
-        if (fighter.health === 0){
-            return console.log(`${fighter.getName()} has dead`);
-        }
-        if(successPercent < randomPercent && fighter.health !== 0){
-            return console.log(`${this.getName()} attack missed`);
-        }else{
-            fighter.health -= this._damage;
-            if (fighter.health === 0){
-                console.log(`${this.getName()} makes ${this.getDamage()} damage to ${fighter.getName()}`);
-                return console.log(`${fighter.getName()} has dead`);
-            }
-            return console.log(`${this.getName()} makes ${this.getDamage()} damage to ${fighter.getName()}`);
-        }
-    }
-    logCombatHistory(){
-        console.log(`Name: ${this.getName()}, Wins: ${this.getWins()}, Losses: ${this.getLosses()}`);
-        }
-    heal(healed) {
-        this._health += healed;
-        if (this._health > this.health){
-            this._health = this.health;
-            return this._health;
-        }else{
-            return this._health;
-        }
-    }
-    dealDamage(damaged) {
-        this._health -= damaged;
-        if (this._health < 0){
-            this._health = 0;
-            return this._health;
-        }else{
-            return this._health;
-        }
-    }
+	constructor(prop) {
+		this.getName = function(){
+			return prop.name;
+		}
+		this.getDamage = function(){
+			return prop.damage;
+		}
+		this.getStrenght = function(){
+			return prop.strenght;
+		}
+		this.getAgility = function(){
+			return prop.agility;
+		}
+		this._health = prop.hp;
+		this._init_hp = prop.hp;
+		this._loss = 0;
+		this._win = 0;
+	}
+	getHealth() {
+		return this._health;
+	}
+	attack(fighter) {
+		const chance = 100 - fighter.getAgility() - fighter.getStrenght();
+		const success = getRandomIntInclusive(0, 100);
+		if (success <= chance) {
+			fighter._health -= this.getDamage();
+		if (fighter._health <= 0) {
+			fighter._health = 0;
+			this.addWin();
+			fighter.addLoss();
+		}
+		return `${this.getName()} makes ${this.getDamage()} damage to ${fighter.getName()}`;
+		} else {
+			return `${fighter.getName()} attack missed`;
+		}
+	}
+	heal(hp) {
+		this._health += hp;
+		if (this._health > this._init_hp) {
+			this._health = this._init_hp;
+		}
+	}
+	dealDamage(hp) {
+		this._health -= hp;
+		if (this._health <= 0) {
+			this._health = 0;
+			this.addLoss();
+		}
+	}
+	addWin() {
+		return ++this._win;
+	}
+	addLoss() {
+		return ++this._loss;
+	}
+	logCombatHistory() {
+		return `Name: ${this.getName()}, Wins: ${this._win}, Losses: ${this._loss}`;
+	}
 }
-function battle(fighter1,fighter2){
-    for(;;){
-        fighter1.attack(fighter2);
-        if(fighter2.health === 0){
-            console.log(`${fighter1.getName()} has won`);
-            fighter2.addWins();
-            fighter1.addLosses();
-            break;
-        }
-        fighter2.attack(fighter1);
-        if(fighter1.health === 0){
-            console.log(`${fighter2.getName()} has won`);
-            fighter1.addWins();
-            fighter2.addLosses();
-            break;
-        }
-    }
+function battle(fighter1, fighter2) {
+	if(comp(fighter1,fighter2)){
+		return `${fighter1.getName()} can't fight with himself`;
+	}
+	if (fighter1.getHealth() === 0) {
+		return `${fighter1.getName()} is dead and can't fight`; 
+	}
+	if (fighter2.getHealth() === 0) {
+		return `${fighter2.getName()} is dead and can't fight`; 
+	}
+	const TRUE = true; 
+	while (TRUE) {
+		console.log(fighter1.attack(fighter2));
+		if (fighter2.getHealth() === 0) {
+			return `${fighter1.getName()} has won`; 
+		}
+		console.log(fighter2.attack(fighter1));
+		if (fighter1.getHealth() === 0) {
+			return `${fighter2.getName()} has won`; 
+		}
+	}
 }
-const fighter1 = new Fighter({name:'Maximus', damage: 25, hp: 100, strength: 30, agility: 25});
-const fighter2 = new Fighter({name:'Comodus', damage: 20, hp: 100, strength: 25, agility: 20});
-battle(fighter1,fighter2);
+function comp(f1, f2){
+	let isEqual = false;
+	if(f1===f2) {
+		isEqual = true; 
+	}
+	return isEqual;
+}
+
+const fighter1 = new Fighter({ name: 'F1', hp: 150, damage: 20, strenght: 15, agility: 10 });
+const fighter2 = new Fighter({ name: 'F2', hp: 70, damage: 60, strenght: 10, agility: 10 });
