409ebb52502ccf67146a0a465b60094f18303a04
WebProjectGroup <WebProjectGroup@i.ua>
2020-06-02 12:36:57 +0300

    HW9

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,119 +1,109 @@
-function convert(){
-    let convertedArray = [];
-    for(let arg = 0; arg < arguments.length; arg++){
-    if(arguments[arg] === String(arguments[arg])){
-       convertedArray.push(Number(arguments[arg]));
-    } else if (arguments[arg] === Number(arguments[arg])){
-        convertedArray.push(String(arguments[arg]));
-       }
-    }
-    return convertedArray;
+function convert(...array){
+	for(let i=0; i<array.length; i++){
+		if(typeof array[i] === 'string'){
+			array[i] = parseInt(array[i]);
+		}else{
+			array[i] = '' + array[i];
+		}
+	}
+	return array;
 }
-function executeforEach(args = [], multiplies){
-    for(let arg = 0; arg < args.length; arg++){
-    args[arg] = multiplies(args[arg]);
-    }
-        return args;
-    }
-function mapArray(args = [], addition){
-    for(let arg = 0; arg < args.length; arg++){
-        if(args[arg] === String(args[arg])){
-            args[arg] -=0;
-            args[arg] = addition(args[arg]);
-        }else{
-            args[arg] = addition(args[arg]);
-            }
-    }
-        return args;
-    }
-function filterArray(args = [], even){
-    let filtredArray = [];
-    even = function name(el){
-    return el % 2 === 0
-    };
-    for(let arg = 0; arg < args.length; arg++){
-        if(even(arg) === true){
-            filtredArray.push(args[arg]);
-        }
-    }
-        return filtredArray;
-    }
-function containsValue(args = [], value){
-    for(let arg = 0; arg < args.length; arg++){
-        if(args[arg] === value){
-            return true;
-        }else{
-            return false;
-        }
-    }
+function executeforEach(array, callback){
+	for(let i=0; i< array.length; i++){
+		callback(array[i], i);
+	}
+}
+function mapArray(array, callback){
+	executeforEach(array, (item, index) => {
+		array[index] = callback(item);
+	});
+	return console.log(array);
+}
+function filterArray(array, callback){
+	let filteredArr = [];
+	executeforEach(array, (item) => {
+		if(callback(item)){
+			filteredArr.push(item);
+		}
+	});
+	return console.log(filteredArr);
+}
+function containsValue(array, number){
+	let contains = false;
+	executeforEach(array, (item, index) => {
+		if(array[index] === number){
+			contains = true;
+		}
+	});
+	return console.log(contains);
 }
 function flipOver(string){
-    string = String(string);
-    let newString = '';
-    for(let char = string.length - 1; char >= 0; char--){
-        newString += string[char];
-        }
-    return newString;
+	let reverseStr = '';
+	executeforEach(string, (item,index) => {
+		reverseStr += string[string.length - index -1];
+	});
+	return console.log(reverseStr);
 }
-function makeListFromRange(numbersRange = []){
-    let secondNum = numbersRange[1];
-    let newArray = [];
-
-    for( let firstNum = numbersRange[0]; firstNum <= secondNum; firstNum++ ){
-        newArray.push(firstNum);
-    }
-    return newArray;
+function makeListFromRange(range){
+	if(range[0]>range[1]){
+		const TEMP = range[0];
+		range[0] = range[1];
+		range[1] = TEMP;
+	}
+	const LENGTH = range[1] - range[0] + 1;
+	let mas = [];
+	for(let i=0; i<LENGTH; i++){
+		mas[i] = range[0] + i;
+	}
+	return console.log(mas);
+}	
+function getArrayOfKeys(obj, key){
+	let arrOfKeys = [];
+	executeforEach(obj, (item) => {
+		arrOfKeys.push(item[key]);
+	});
+	console.log(arrOfKeys);
 }
-function getArrayOfKeys(array = [],keyName){
-    let keysArray = [];
-    for(let key = 0; key < array.length; key++){
-        keysArray.push(array[key][keyName]);
-    }
-    return keysArray;
+function substitute(arr){
+	const range = [10,20];
+	mapArray(arr, (item) => {
+		if(item<range[1] && item>range[0]){
+			item = '*';	
+		}
+		return item;
+	});
+	return arr;
 }
-function substitute(array =[]){
-    for(let number = 0; number < array.length; number++){
-        if(array[number] < 20 && array[number] > 10){
-            array[number] = '*';
-        }
-    }    
-    return array;
-}
-function getPastDay(inputDate, day){
-    let date = new Date(inputDate);
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'];
-    let options = {
-        year: 'numeric',
-        months: 'long',
-        day: 'numeric',
-        timezone: 'UTC'
-      };
-      date.setDate(date.getDate() - day);
-    let result = `${date.getDate()},(${date.getDate()} ${monthNames[date.getMonth(date.toLocaleString('en-US',options))]} ${date.getFullYear()})`;
-    return result;
+function getPastDay(date, daysAgo){
+	const newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()-daysAgo);
+	const formatter = new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+	});
+	return console.log(formatter.format(newDate));
 }
 function formatDate(date){
-    const dateFormat = 10;
-   let days = date.getDate();
-   let months = date.getMonth() + 1;
-   let years = date.getFullYear();
-   let hours = date.getHours();
-   let minutes = date.getMinutes();
-   if(days < dateFormat) {
-      days = '0' + days;
-    }
-   if(months < dateFormat) {
-      months = '0' + months;
-    }
-   if(years < dateFormat) {
-      years = '0' + years;
-    }
-   if(hours < dateFormat) {
-      hours = '0' + hours;
-    }
-   if(minutes < dateFormat) {
-      minutes = '0' + minutes;
-   }
-   return years + '/' + months + '/' + days + ' ' + hours + ':' + minutes;
+	let dd = date.getDate();
+	const TEN = 10;
+	if (dd < TEN) {
+		dd = '0' + dd;
+	}
+	let mm = date.getMonth() + 1;
+	if (mm < TEN) {
+		mm = '0' + mm;
+	}
+	let yy = date.getFullYear();
+	if (yy < TEN) {
+		yy = '0' + yy;
+	}
+	let hh = date.getHours();
+	if (hh < TEN) {
+		hh = '0' + hh;
+	} 
+	let min = date.getMinutes();
+	if (min < TEN) {
+		min = '0' + min;
+	} 
+	return console.log(yy + '/' + mm + '/' + dd + ' ' + hh + ':' + min);
 }
